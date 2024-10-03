6c386f04eb17a760b72684df432c7b73eeb4c3e1
Ray Cooke <rcjcooke@gmail.com>
2024-10-03 21:34:13 +0100

    Future notes for water supply controller

---
+++ software/nutrient_ctrl/src/WaterSupplyController.hpp
@@ -0,0 +1,2 @@
+// Add in weekly 2-3 minute flush (need to add solenoid in parallel to manual flush valve) - This is to ensure that the RO membranes are flushed to remove impurities and bacteria left on the surface of the membrane.
+// Add TDS sensors to detect when the filters and membrane need to be replaced
