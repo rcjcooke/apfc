32319e21224b91640e89b06e198df8a05b4a9106
Ray Cooke <rcjcooke@gmail.com>
2024-10-17 20:51:54 +0100

    Formatting / comments

--- software/lib/A02YYUWDistanceSensor/A02YYUWDistanceSensor.cpp
+++ software/lib/A02YYUWDistanceSensor/A02YYUWDistanceSensor.cpp
@@ -46,10 +46,11 @@ void A02YYUWDistanceSensor::setProcessed(bool processed) {
 /*******************************
  * Actions
  *******************************/
-// Reads the distance from the sensor (returns -1 if there's a checksum error, -2 if the frame wasn't read correctly)
+// Reads the distance from the sensor (returns 0 if successful, -1 if there's a checksum error, -2 if the frame wasn't read correctly)
 int A02YYUWDistanceSensor::readDistance() {
   // Note: There's a minimum 100ms between readings at best, so don't read if it's not been 100ms since the last correctly formatted reading
   if (millis() > mNextReadTime) {
+    // Frame format (1 byte each): Header, DATA_H, DATA_L, SUM
     do {
       for (int i = 0; i < 4; i++) {
         mBuffer[i] = mSensorUART->read();
@@ -66,10 +67,12 @@ int A02YYUWDistanceSensor::readDistance() {
         mLastMeasuredDistance = (mBuffer[1] << 8) + mBuffer[2];
         mNextReadTime = mNextReadTime + 100;
         return 0;
-      } else
+      } else {
         return -1;
-    } else
+      }
+    } else {
       return -2;
+    }
   } else {
     // Return as if it was a successful read
     return 0;
