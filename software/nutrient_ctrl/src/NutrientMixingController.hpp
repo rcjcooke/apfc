233982795b2e255df637e55c11686967c497d308
Ray Cooke <rcjcooke@gmail.com>
2025-07-23 12:14:17 +0100

    Started TODO list for Nutrient Mixing

---
+++ software/nutrient_ctrl/src/NutrientMixingController.hpp
@@ -0,0 +1,6 @@
+// Measure Electrolyte Concentration
+// Determine total system solution volume
+// Calculate additional water volume based on total system volume and top up accordingly
+// Calculate nutrient solution topup volume based on target nutrient solution concentration and top up accordingly
+// Mix the solution regularly (to ensure even distribution of nutrients and heat as well as regular sterilisation of the solution)
+// 
